76c5666da881c2270f5d57c074b661324bea0bb4
Jerry W Jackson <jerry.w.jackson@gmail.com>
2023-12-20 13:15:59 -0600

    create project structure and setup express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+})
